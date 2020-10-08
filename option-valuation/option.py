179b67c5907416e2dda1d7cd125113066a44a9a4
romaincaraes <hello@romaincaraes.fr>
2020-10-08 17:28:54 +0200

    Move app.py code into option.py

--- option-valuation/option.py
+++ option-valuation/option.py
@@ -5,6 +5,7 @@ import time, datetime
 import numpy as np
 import pandas as pd
 import scipy.stats as sp
+import streamlit as st
 import models.binomial as bn
 import models.blackscholes as bs
 import models.montecarlo as mc
@@ -98,6 +99,59 @@ class Put(Option) :
     def __init__(self, ul_asset, strike, expiry, style) :
         Option.__init__(self, ul_asset, type, strike, expiry, style)
         self.type = "put"
+        
+model = {1:"Binomial", 2:"Black & Scholes", 3:"Monte Carlo"}
+model = st.sidebar.selectbox("Model", list(model.keys()), 1, lambda x: model[x])
+
+def user_input_features() :
+    st.sidebar.header("Option")
+    type = st.sidebar.selectbox("Type", ["Call", "Put"])
+    ul_asset = st.sidebar.text_input("Underlying Asset", "EURUSD")
+    spot = st.sidebar.number_input("Spot", 0.0000, 2.0000, 1.1850, step=0.0005, format="%.4f")
+    strike = st.sidebar.number_input("Strike", 0.0000, 2.0000, 1.1650, step=0.0005, format="%.4f")
+    expiry = st.sidebar.date_input("Expiry", datetime.date(2020, 12, 31))
+    style = st.sidebar.selectbox("Style", ["EU"])
+    
+    st.sidebar.header("Market")
+    riskfree = st.sidebar.slider("Riskfree Rate", 0.00, 10.00, 1.00, step=0.05)
+    volatility = st.sidebar.slider("Implied Volatility", 0.00, 1.00, 0.20, step=0.01)
+
+    data = {
+        "type" : type.lower(),
+        "ul_asset" : ul_asset,
+        "strike" : strike,
+        "spot" : spot,
+        "expiry" : expiry.strftime("%Y-%m-%d"),
+        "style" : style,
+        "riskfree" : riskfree,
+        "volatility" : volatility
+    }
+    features = pd.DataFrame(data, index=[0])
+    return features
+
+st.subheader("User Input Parameters")    
+features = user_input_features()
+st.write(features)
+
+spot = features['spot'][0]
+riskfree = features['riskfree'][0]
+volatility = features['volatility'][0]
+
+option = Option(
+    ul_asset=features['ul_asset'][0],
+    type=features['type'][0],
+    strike=features['strike'][0],
+    expiry=features['expiry'][0],
+    style=features['style'][0]
+)
+
+st.subheader("Output")
+payoff = {"payoff" : float(option.get_payoff(spot))}
+price = {"price" : float(option.get_price(model, spot, riskfree, 0.0, volatility))}
+greeks = option.get_greeks(spot, riskfree, 0.0, volatility)
+output = {**payoff, **price, **greeks}
+df = pd.DataFrame(output, index=[0])
+st.write(df)
     
 def main() :
     pass
