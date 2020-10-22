78cf8b0d88a1bdaddccc06958cd7487794ec3ac1
romaincaraes <hello@romaincaraes.fr>
2020-10-22 20:37:53 +0200

    Update models/binomial.py
    Compute underlying asset prices tree

--- option-valuation/models/binomial.py
+++ option-valuation/models/binomial.py
@@ -21,6 +21,12 @@ def price(option, spot, riskfree, dividend, volatility) :
     p = (np.exp(riskfree * dt) - d) / (u - d)
 
     ul_price = np.zeros([steps + 1, steps + 1])
+    ul_price[0, 0] = spot
+    for i in range(1, steps + 1) :
+        ul_price[i, 0] = ul_price[i - 1, 0] * u
+        for j in range(1, i + 1) :
+            ul_price[i, j] = ul_price[i - 1, j - 1] * d
+
     option_price = np.zeros([steps + 1, steps + 1])
 
     return option_price[0, 0]
