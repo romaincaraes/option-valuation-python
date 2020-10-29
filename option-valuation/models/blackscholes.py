f515ced0b8f2c21d233c3e1680122026e02c2061
romaincaraes <hello@romaincaraes.fr>
2020-10-29 20:45:26 +0100

    Update models/blackscholes.py
    Fix spaces

--- option-valuation/models/blackscholes.py
+++ option-valuation/models/blackscholes.py
@@ -5,18 +5,18 @@ import numpy as np
 import scipy.stats as sp
 
 def d1(option, spot, riskfree, dividend, volatility) :
-    t = option.days_to_expiry()/365
-    d1 = (np.log(spot/option.strike) + ((riskfree - dividend) + 0.5 * volatility ** 2) * t) / (volatility * np.sqrt(t))
+    t = option.days_to_expiry() / 365
+    d1 = (np.log(spot / option.strike) + ((riskfree - dividend) + 0.5 * volatility ** 2) * t) / (volatility * np.sqrt(t))
     return d1
 
 def d2(option, spot, riskfree, dividend, volatility) :
-    t = option.days_to_expiry()/365
+    t = option.days_to_expiry() / 365
     d2 = d1(option, spot, riskfree, dividend, volatility) - volatility * np.sqrt(t)
     return d2
 
 def price(option, spot, riskfree, dividend, volatility) :
     cdf = sp.norm(0, 1).cdf
-    t = option.days_to_expiry()/365
+    t = option.days_to_expiry() / 365
     if (option.type == "call") :
         price = spot * np.exp(-dividend * t) * cdf(d1(option, spot, riskfree, dividend, volatility)) - option.strike * np.exp(-riskfree * t) * cdf(d2(option, spot, riskfree, dividend, volatility))
     elif (option.type == "put") :
