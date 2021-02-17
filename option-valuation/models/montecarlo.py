3de849072d8f81188b24f143dfb88e954403bcb8
romaincaraes <hello@romaincaraes.fr>
2021-02-17 22:02:57 +0100

    Update models/montecarlo.py

--- option-valuation/models/montecarlo.py
+++ option-valuation/models/montecarlo.py
@@ -5,7 +5,16 @@ import numpy as np
 import scipy.stats as sp
 
 def price(option, spot, riskfree, dividend, volatility, steps=100, simulations=100000) :
-    return 0
+    T = 1.
+    dt = T / steps
+
+    S = spot * np.exp(np.cumsum((riskfree - 0.5 * volatility ** 2) * dt + volatility * np.sqrt(dt) * np.random.standard_normal((steps + 1, simulations)), axis=0))
+
+    S[0] = spot
+
+    price = np.exp(-riskfree * T) * np.sum(np.maximum(S[-1] - option.strike, 0)) / simulations
+
+    return price
 
 def main() :
     pass
