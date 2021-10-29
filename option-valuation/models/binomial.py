e921757681164fbc32950ceab9f5f21e693859bc
romaincaraes <hello@romaincaraes.fr>
2021-10-29 21:39:52 +0200

    Remove whitespaces before colons (PEP8)

--- option-valuation/models/binomial.py
+++ option-valuation/models/binomial.py
@@ -3,27 +3,27 @@
 
 import numpy as np
 
-def up(volatility, duration) :
+def up(volatility, duration):
     u = np.exp(volatility * np.sqrt(duration))
     return u
 
-def down(volatility, duration) :
+def down(volatility, duration):
     d = np.exp(-volatility * np.sqrt(duration))
     return d
 
-def probability_u(volatility, duration, riskfree) :
+def probability_u(volatility, duration, riskfree):
     u = up(volatility, duration)
     d = down(volatility, duration)
     probability_u = (np.exp(riskfree * duration) - d) / (u - d)
     return probability_u
 
-def probability_d(volatility, duration, riskfree) :
+def probability_d(volatility, duration, riskfree):
     u = up(volatility, duration)
     d = down(volatility, duration)
     probability_d = (np.exp(-riskfree * duration) - d) / (u - d)
     return probability_d
 
-def price(option, spot, riskfree, dividend, volatility, steps=4) :
+def price(option, spot, riskfree, dividend, volatility, steps=4):
     dt = option.days_to_expiry() / (365 * steps)
 
     u = up(volatility, dt)
@@ -34,32 +34,32 @@ def price(option, spot, riskfree, dividend, volatility, steps=4) :
 
     ul_price = np.zeros([steps + 1, steps + 1])
     ul_price[0, 0] = spot
-    for i in range(1, steps + 1) :
+    for i in range(1, steps + 1):
         ul_price[i, 0] = ul_price[i - 1, 0] * u
-        for j in range(1, i + 1) :
+        for j in range(1, i + 1):
             ul_price[i, j] = ul_price[i - 1, j - 1] * d
 
     option_price = np.zeros([steps + 1, steps + 1])
-    for j in range(steps + 1) :
-        if option.type == "call" :
+    for j in range(steps + 1):
+        if option.type == "call":
             option_price[steps, j] = max(0, ul_price[steps, j] - option.strike)
-        elif option.type == "put" :
+        elif option.type == "put":
             option_price[steps, j] = max(0, option.strike - ul_price[steps, j])
 
-    for i in range(steps)[::-1] :
-        for j in range(i + 1) :
-            if option.style == "EU" :
+    for i in range(steps)[::-1]:
+        for j in range(i + 1):
+            if option.style == "EU":
                 option_price[i, j] = np.exp(-riskfree * dt) * (p * option_price[i + 1, j] + q * option_price[i + 1, j + 1])
-            elif option.style == "US" :
-                if option.type == "call" :
+            elif option.style == "US":
+                if option.type == "call":
                     option_price[i, j] = max(ul_price[i, j] - option.strike, np.exp(-riskfree * dt) * (p * option_price[i + 1, j] + q * option_price[i + 1, j + 1])) 
-                elif option.type == "put" :
+                elif option.type == "put":
                     option_price[i, j] = max(option.strike - ul_price[i, j], np.exp(-riskfree * dt) * (p * option_price[i + 1, j] + q * option_price[i + 1, j + 1]))
 
     return option_price[0, 0]
 
-def main() :
+def main():
     pass
 
-if __name__ == "__main__" :
+if __name__ == "__main__":
     main()
--- option-valuation/option.py
+++ option-valuation/option.py
@@ -10,113 +10,113 @@ import models.binomial as bn
 import models.blackscholes as bs
 import models.montecarlo as mc
 
-class Option() :
-    def __init__(self, ul_asset, type, strike, expiry, style="EU") :
+class Option():
+    def __init__(self, ul_asset, type, strike, expiry, style="EU"):
         self.type = type
         self.ul_asset = ul_asset
         self.strike = strike
         self.expiry = expiry
         self.style = style
 
-    def days_to_expiry(self) :
+    def days_to_expiry(self):
         today = datetime.datetime.now()
         expiry = datetime.datetime.strptime(self.expiry, "%Y-%m-%d")
         days_to_expiry = (expiry - today).days
         return days_to_expiry
 
-    def get_payoff(self, spot) :
-        if (self.type == "call") :
+    def get_payoff(self, spot):
+        if (self.type == "call"):
             payoff = np.maximum(0, spot - self.strike)
-        elif (self.type == "put") :
+        elif (self.type == "put"):
             payoff = np.maximum(0, self.strike - spot)
         return payoff
 
-    def get_price(self, model, **kwargs) :
-        if (model == 1) :
+    def get_price(self, model, **kwargs):
+        if (model == 1):
             price = bn.price(self, spot=kwargs['spot'], riskfree=kwargs['riskfree'], dividend=kwargs['dividend'], volatility=kwargs['volatility'], steps=kwargs['steps'])
-        elif (model == 2) :
+        elif (model == 2):
             price = bs.price(self, spot=kwargs['spot'], riskfree=kwargs['riskfree'], dividend=kwargs['dividend'], volatility=kwargs['volatility'])
-        elif (model == 3) : 
+        elif (model == 3): 
             price = mc.price(self, spot=kwargs['spot'], riskfree=kwargs['riskfree'], dividend=kwargs['dividend'], volatility=kwargs['volatility'], steps=kwargs['steps'], simulations=kwargs['simulations'])
         return price
 
-    def snpdf(self, spot, riskfree, dividend, volatility) :
+    def snpdf(self, spot, riskfree, dividend, volatility):
         snpdf = (1 / (np.sqrt(2 * np.pi))) * (np.exp((-bs.d1(self, spot, riskfree, dividend, volatility) ** 2) / 2))
         return snpdf
 
-    def get_delta(self, spot, riskfree, dividend, volatility) :
+    def get_delta(self, spot, riskfree, dividend, volatility):
         cdf = sp.norm(0, 1).cdf
         t = self.days_to_expiry() / 365
-        if (self.type == "call") :
+        if (self.type == "call"):
             delta = np.exp(-dividend * t) * cdf(bs.d1(self, spot, riskfree, dividend, volatility))
-        elif (self.type == "put") :
+        elif (self.type == "put"):
             delta = np.exp(-dividend * t) * (cdf(bs.d1(self, spot, riskfree, dividend, volatility)) - 1)
         return delta
 
-    def get_gamma(self, spot, riskfree, dividend, volatility) :
+    def get_gamma(self, spot, riskfree, dividend, volatility):
         t = self.days_to_expiry() / 365
         gamma = (np.exp(-dividend * t) / (spot * volatility * np.sqrt(t))) * self.snpdf(spot, riskfree, dividend, volatility)
         return gamma
 
-    def get_vega(self, spot, riskfree, dividend, volatility) :
+    def get_vega(self, spot, riskfree, dividend, volatility):
         t = self.days_to_expiry() / 365
         vega = (0.01 * spot * np.exp(-dividend * t) * np.sqrt(t)) * self.snpdf(spot, riskfree, dividend, volatility)
         return vega
 
-    def get_theta(self, spot, riskfree, dividend, volatility) :
+    def get_theta(self, spot, riskfree, dividend, volatility):
         cdf = sp.norm(0, 1).cdf
         t = self.days_to_expiry() / 365
-        if (self.type == "call") :
+        if (self.type == "call"):
             theta = (-(((spot * volatility * np.exp(-dividend * t)) / (2 * np.sqrt(t))) * self.snpdf(spot, riskfree, dividend, volatility)) - (riskfree * self.strike * np.exp(-riskfree * t) * cdf(bs.d2(self, spot, riskfree, dividend, volatility))) + (dividend * spot * np.exp(-dividend * t) * cdf(bs.d1(self, spot, riskfree, dividend, volatility)))) / 365
-        elif (self.type == "put") :
+        elif (self.type == "put"):
             theta = (-(((spot * volatility * np.exp(-dividend * t)) / (2 * np.sqrt(t))) * self.snpdf(spot, riskfree, dividend, volatility)) + (riskfree * self.strike * np.exp(-riskfree * t) * cdf(-bs.d2(self, spot, riskfree, dividend, volatility))) - (dividend * spot * np.exp(-dividend * t) * cdf(-bs.d1(self, spot, riskfree, dividend, volatility)))) / 365
         return theta
 
-    def get_rho(self, spot, riskfree, dividend, volatility) :
+    def get_rho(self, spot, riskfree, dividend, volatility):
         cdf = sp.norm(0, 1).cdf
         t = self.days_to_expiry() / 365
-        if (self.type == "call") :
+        if (self.type == "call"):
             rho = 0.01 * self.strike * t * np.exp(-riskfree * t) * cdf(bs.d2(self, spot, riskfree, dividend, volatility))
-        elif (self.type == "put") :
+        elif (self.type == "put"):
             rho = -0.01 * self.strike * t * np.exp(-riskfree * t) * cdf(-bs.d2(self, spot, riskfree, dividend, volatility))
         return rho
 
-    def get_greeks(self, spot, riskfree, dividend, volatility) :
+    def get_greeks(self, spot, riskfree, dividend, volatility):
         greeks = {
-            "delta" : self.get_delta(spot, riskfree, dividend, volatility),
-            "gamma" : self.get_gamma(spot, riskfree, dividend, volatility),
-            "vega" : self.get_vega(spot, riskfree, dividend, volatility),
-            "theta" : self.get_theta(spot, riskfree, dividend, volatility),
-            "rho" : self.get_rho(spot, riskfree, dividend, volatility)
+            "delta": self.get_delta(spot, riskfree, dividend, volatility),
+            "gamma": self.get_gamma(spot, riskfree, dividend, volatility),
+            "vega": self.get_vega(spot, riskfree, dividend, volatility),
+            "theta": self.get_theta(spot, riskfree, dividend, volatility),
+            "rho": self.get_rho(spot, riskfree, dividend, volatility)
         }
         return greeks
 
-class Call(Option) :
-    def __init__(self, ul_asset, strike, expiry, style) :
+class Call(Option):
+    def __init__(self, ul_asset, strike, expiry, style):
         Option.__init__(self, ul_asset, type, strike, expiry, style)
         self.type = "call"
 
-class Put(Option) :
-    def __init__(self, ul_asset, strike, expiry, style) :
+class Put(Option):
+    def __init__(self, ul_asset, strike, expiry, style):
         Option.__init__(self, ul_asset, type, strike, expiry, style)
         self.type = "put"
 
 model = {1:"Binomial", 2:"Black & Scholes", 3:"Monte Carlo"}
 model = st.sidebar.selectbox(label="Model", options=list(model.keys()), index=1, format_func=lambda x: model[x])
 
-if (model == 1) : # Binomial
+if (model == 1): # Binomial
     st.sidebar.header("Model Parameters")
     steps = st.sidebar.number_input(label="Steps", min_value=1, value=4)
     simulations = 0
-elif (model == 2) : # Black & Scholes
+elif (model == 2): # Black & Scholes
     steps = 0
     simulations = 0
-elif (model == 3) : # Monte Carlo
+elif (model == 3): # Monte Carlo
     st.sidebar.header("Model Parameters")
     steps = st.sidebar.number_input(label="Steps", min_value=1, value=100)
     simulations = st.sidebar.number_input(label="Simulations", min_value=1, max_value=100000, value=100000)
 
-def user_input_features() :
+def user_input_features():
     st.sidebar.header("Option")
     type = st.sidebar.selectbox(label="Type", options=["Call", "Put"])
     ul_asset = st.sidebar.text_input(label="Underlying Asset", value="EURUSD")
@@ -131,15 +131,15 @@ def user_input_features() :
     volatility = st.sidebar.slider(label="Implied Volatility", min_value=0.0, max_value=1.0, value=0.2, step=0.0005)
 
     data = {
-        "type" : type.lower(),
-        "ul_asset" : ul_asset,
-        "strike" : strike,
-        "spot" : spot,
-        "expiry" : expiry.strftime("%Y-%m-%d"),
-        "style" : style,
-        "riskfree" : riskfree,
-        "dividend" : dividend,
-        "volatility" : volatility
+        "type": type.lower(),
+        "ul_asset": ul_asset,
+        "strike": strike,
+        "spot": spot,
+        "expiry": expiry.strftime("%Y-%m-%d"),
+        "style": style,
+        "riskfree": riskfree,
+        "dividend": dividend,
+        "volatility": volatility
     }
     features = pd.DataFrame(data, index=[0])
     return features
@@ -165,14 +165,14 @@ option = Option(
 )
 
 st.header("Output")
-payoff = {"payoff" : float(option.get_payoff(spot=spot))}
-price = {"price" : float(option.get_price(model=model, spot=spot, riskfree=riskfree, dividend=dividend, volatility=volatility, steps=steps, simulations=simulations))}
+payoff = {"payoff": float(option.get_payoff(spot=spot))}
+price = {"price": float(option.get_price(model=model, spot=spot, riskfree=riskfree, dividend=dividend, volatility=volatility, steps=steps, simulations=simulations))}
 greeks = option.get_greeks(spot=spot, riskfree=riskfree, dividend=dividend, volatility=volatility)
 output = {**payoff, **price, **greeks}
 df = pd.DataFrame(output, index=[0])
 st.write(df)
 
-def debug() :
+def debug():
     start = time.time()
     spot = 100.
 
@@ -195,8 +195,8 @@ def debug() :
     print(f"MC. {p_mc:.6f} ({t_mc:.6f} secs)")
     return
 
-def main() :
+def main():
     pass
 
-if __name__ == "__main__" :
+if __name__ == "__main__":
     main()
