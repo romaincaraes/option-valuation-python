40a5f28a0ebeaa00bc9c84f7dc1353c0cfe1dae8
romaincaraes <hello@romaincaraes.fr>
2021-11-16 23:40:39 +0100

    Add test_option() in tests/test_option.py

--- option-valuation/tests/test_option.py
+++ option-valuation/tests/test_option.py
@@ -1,6 +1,10 @@
 # test_option.py
 
-from option import Call, Put
+from option import *
+
+def test_option():
+    o = Option("INDEX", "call", 100., "2021-12-31", "EU")
+    assert True
 
 def test_option_call():
     c = Call("INDEX", 100., "2021-12-31", "EU")
