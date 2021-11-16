0405058154c7a4213085b4d7ec7a2d71ceba02d0
romaincaraes <hello@romaincaraes.fr>
2021-11-16 23:03:47 +0100

    Add test_option_put() in tests/test_option.py

--- option-valuation/tests/test_option.py
+++ option-valuation/tests/test_option.py
@@ -5,3 +5,7 @@ from option import Call, Put
 def test_option_call():
     c = Call("INDEX", 100., "2021-12-31", "EU")
     assert True
+
+def test_option_put():
+    p = Put("INDEX", 100., "2021-12-31", "EU")
+    assert True
