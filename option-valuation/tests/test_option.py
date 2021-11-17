b34a498b412230727ada78e5c997759553d80610
romaincaraes <hello@romaincaraes.fr>
2021-11-17 22:55:10 +0100

    Update tests/test_option.py

--- option-valuation/tests/test_option.py
+++ option-valuation/tests/test_option.py
@@ -3,13 +3,13 @@
 from option import *
 
 def test_option():
-    o = Option("INDEX", "call", 100., "2021-12-31", "EU")
+    o = Option(ul_asset="INDEX", type="call", strike=100., expiry="2021-12-31", style="EU")
     assert True
 
 def test_option_call():
-    c = Call("INDEX", 100., "2021-12-31", "EU")
+    c = Call(ul_asset="INDEX", strike=100., expiry="2021-12-31", style="EU")
     assert True
 
 def test_option_put():
-    p = Put("INDEX", 100., "2021-12-31", "EU")
+    p = Put(ul_asset="INDEX", strike=100., expiry="2021-12-31", style="EU")
     assert True
