0d4d0e3f69d647ca1c8c594b3da85ea39266e54d
romaincaraes <hello@romaincaraes.fr>
2022-03-19 09:40:04 +0100

    Add test_option_call() and 'call', 'spot', 'riskfree', 'dividend' and 'volatility' fixtures

--- option-valuation/tests/conftest.py
+++ option-valuation/tests/conftest.py
@@ -13,3 +13,33 @@ def option():
         style="EU"
     )
     return option
+
+@pytest.fixture
+def call():
+    call = Call(
+        ul_asset="UL_ASSET",
+        strike=random.randint(0,10e12),
+        expiry=(datetime.datetime.now() + datetime.timedelta(days=random.randint(0,36500))).strftime("%Y-%m-%d"),
+        style="EU"
+    )
+    return call
+
+@pytest.fixture
+def spot(option):
+    spot = option.strike + random.random() * random.choice((-1, 1))
+    return spot
+
+@pytest.fixture
+def riskfree():
+    riskfree = random.random()
+    return riskfree
+
+@pytest.fixture
+def dividend():
+    dividend = random.random()
+    return dividend
+
+@pytest.fixture
+def volatility():
+    volatility = random.random()
+    return volatility
--- option-valuation/tests/test_option.py
+++ option-valuation/tests/test_option.py
@@ -4,3 +4,8 @@ from option import *
 
 def test_option(option):
     assert isinstance(option, Option)
+
+def test_option_call(call, spot, riskfree, dividend, volatility):
+    assert isinstance(call.days_to_expiry(), int)
+    assert isinstance(call.get_payoff(spot), float)
+    assert isinstance(call.get_delta(spot, riskfree, dividend, volatility), float)
