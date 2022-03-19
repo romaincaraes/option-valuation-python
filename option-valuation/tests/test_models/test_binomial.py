791aebb4f48571ab86c3204c44d9067676367c3a
romaincaraes <hello@romaincaraes.fr>
2022-03-19 13:12:33 +0100

    Add test_binomial_probability_d()

--- option-valuation/tests/test_models/test_binomial.py
+++ option-valuation/tests/test_models/test_binomial.py
@@ -11,3 +11,6 @@ def test_binomial_down(volatility, steps):
 
 def test_binomial_probability_u(volatility, riskfree, steps):
     assert bn.probability_u(volatility, 60/(365*steps), riskfree)
+
+def test_binomial_probability_d(volatility, riskfree, steps):
+    assert bn.probability_d(volatility, 60/(365*steps), riskfree)
