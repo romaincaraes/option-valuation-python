095d4c90fa4fc330d16b898b553363eb96cae050
romaincaraes <hello@romaincaraes.fr>
2022-03-19 13:13:51 +0100

    Add test_binomial_price()

--- option-valuation/tests/test_models/test_binomial.py
+++ option-valuation/tests/test_models/test_binomial.py
@@ -14,3 +14,7 @@ def test_binomial_probability_u(volatility, riskfree, steps):
 
 def test_binomial_probability_d(volatility, riskfree, steps):
     assert bn.probability_d(volatility, 60/(365*steps), riskfree)
+
+def test_binomial_price(call, put, spot, riskfree, dividend, volatility, steps):
+    assert bn.price(call, spot, riskfree, dividend, volatility, steps)
+    assert bn.price(put, spot, riskfree, dividend, volatility, steps)
