8af32df5b92a1463209d7826affeaf92adaaf069
romaincaraes <hello@romaincaraes.fr>
2022-03-19 13:18:58 +0100

    Add test_blackscholes_d1()

--- option-valuation/tests/test_models/test_blackscholes.py
+++ option-valuation/tests/test_models/test_blackscholes.py
@@ -1,3 +1,8 @@
 # test_blackscholes.py
 
+from option import *
 import models.blackscholes as bs
+
+def test_blackscholes_d1(call, put, spot, riskfree, dividend, volatility):
+    assert bs.d1(call, spot, riskfree, dividend, volatility)
+    assert bs.d1(put, spot, riskfree, dividend, volatility)
